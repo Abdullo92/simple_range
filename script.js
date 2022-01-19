3f1ab5e1d299bdc82bf1aca64174b3ca8a850d1b
Abdullo92 <abdullo.giyasov@gmail.com>
2022-01-19 19:06:59 +0500

    simple range

---
+++ script.js
@@ -0,0 +1,15 @@
+function f1() {
+    let ran1 = document.querySelector('#ran1').value
+    let ran2 = document.querySelector('#ran2').value
+    let ran3 = document.querySelector('#ran3').value
+    let ran4 = document.querySelector('#ran4').value
+    let text1 = document.querySelector('#text1');
+    let text2 = document.querySelector('#text2');
+    let text3 = document.querySelector('#text3');
+    let text4 = document.querySelector('#text4');
+
+   text1.value=ran1
+   text2.value=ran2
+   text3.value=ran3
+   text4.value=ran4
+}
